d0bcdfcf595697f6aa60e0f28fd30760fcc00481
hhw12409 <sksk2425@naver.com>
2022-09-08 21:01:35 +0900

    Feat : setting Recoil

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,67 @@ import React, { useEffect, useState } from "react";
 import Router from "./Router";
 import GlobalStyle from "./GlobalStyle";
 import { auth } from "./fbInstance";
-import { onAuthStateChanged, User } from "firebase/auth";
+import { onAuthStateChanged, updateProfile, User } from "firebase/auth";
+import { useRecoilState } from "recoil";
+import { loginCheck } from "./atoms/loginCheck";
+
+interface IArgs {
+  displayName?: string | null;
+  photoURL?: string | null;
+}
+interface IProps {
+  updateProfile?: (args: IArgs) => Promise<void>;
+}
 
 function App() {
   const [init, setInit] = useState(false);
-  const [userObj, setUserObj] = useState<any>(null);
+  const [userObj, setUserObj] = useState<User | IProps | null>();
+  const [isLogin, setIsLogin] = useRecoilState(loginCheck);
 
   useEffect(() => {
     // 로그인, 로그아웃 여부 확인
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        setUserObj(user as any);
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => updateProfile(user, args),
+        });
+      } else {
+        setUserObj(null);
       }
       setInit(true);
+      setIsLogin(true);
     });
   }, []);
+  const refreshUser = () => {
+    const user = auth.currentUser;
+    if (user !== null) {
+      setUserObj({
+        displayName: user.displayName,
+        uid: user.uid,
+        updateProfile: (args) => updateProfile(user, args),
+      });
+    } else {
+      setUserObj(null);
+    }
+  };
   return (
     <>
-      <GlobalStyle />
-      {init ? <Router isLogin={Boolean(userObj)} userObj={userObj} /> : "Loading..."}
+      <div
+        style={{
+          maxWidth: 890,
+          width: "100%",
+          margin: "50px auto",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <GlobalStyle />
+        {init ? <Router refreshUser={refreshUser} isLogin={isLogin} userObj={userObj} /> : "Loading..."}
+      </div>
     </>
   );
 }
