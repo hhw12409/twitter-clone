07be8d1e8b0ed844708691152a6cd2cec2b73547
hhw12409 <sksk2425@naver.com>
2022-09-11 00:27:04 +0900

    Feat: router setting

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,23 +6,24 @@ import Navigation from "./components/Navigation";
 import Profile from "./routes/Profile";
 import NotFound from "./routes/NotFound";
 import { User } from "firebase/auth";
-// import { User } from "firebase/auth";
+import { useRecoilValue } from "recoil";
+import { loginCheck } from "./atoms/loginCheck";
 
 interface IProps {
-  // isLogin: User | null;
-  isLogin: boolean;
   userObj: User;
+  refreshUser: () => void;
 }
 
-const Router = ({ isLogin, userObj }: IProps) => {
+const Router = ({ userObj, refreshUser }: any) => {
+  const isLogin = useRecoilValue(loginCheck);
   return (
     <BrowserRouter>
-      {isLogin && <Navigation />}
+      {isLogin && <Navigation userObj={userObj} />}
       <Routes>
         {isLogin ? (
           <>
             <Route path="/" element={<Home userObj={userObj} />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile" element={<Profile userObj={userObj} refreshUser={refreshUser} />} />
             <Route path="*" element={<NotFound />} />
           </>
         ) : (
